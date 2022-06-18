a7cbdbd13634616aae13d99ae4cc2f7e7de1981c
beca_d <67708213+Beca-d@users.noreply.github.com>
2022-06-18 17:29:28 -0400

    token returned

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,19 @@
-const APIController = (function() {
+const clientId = '0a772d470d6b4bc295787c115dc84e0e';
+const clientSecret = '51705cf2746340e3b66493d943e6eb05';
 
-    const clientId = '0a772d470d6b4bc295787c115dc84e0e';
-    const clientSecret = '51705cf2746340e3b66493d943e6eb05';
+const clientValid = (btoa(clientId + ':' + clientSecret))
+console.log(clientValid)
 
-    const _getToken = async () => {
-
-        const result = await fetch('https://accounts.spotify.com/api/token', {
-            method: 'POST',
-            headers: {
-                'Content-Type' : 'application/x-www-form-urlencoded',
-                'Authorization' : 'Basic' + btoa(clientId + ':' + clientSecret)
-            },
-            body: 'grant_type=client_cedentials'
-        });
-
-        const data = await result.json();
-        return data.access_token;
-    }
-
-   /* const _getTrack = async (token, tracksEndPoint) => {
-
-        const limit = 10;
-    
-        const result = await fetch('$(tracksEndPoint)?limit-$(limit)', {
-            method: 'GET',
-            headers: {'Authorization' : 'Bearer' + token},
-        });
-
-        const data = await result.json();
-        return data.items;
-    }
-
-    return {
-        getToken() {
-            return _getToken
-        },
-    } */
-});
-APIController ();
-_getToken ();
-console.log(token);
+fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    headers: {
+        'Content-Type' : 'application/x-www-form-urlencoded',
+        'Authorization' : 'Basic ' + clientValid
+    },
+    body: 'grant_type=client_credentials'
+})
+    .then(res => {
+        return res.json()
+    })
+    .then(data => console.log(data))
+    .catch(error => console.log('ERROR'))
