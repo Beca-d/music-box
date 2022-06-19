11d76f248ee0232a84a4f8163d5ade28a6b6eed3
beca_d <67708213+Beca-d@users.noreply.github.com>
2022-06-19 01:02:38 -0400

    ready to merge

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ const clientSecret = '51705cf2746340e3b66493d943e6eb05';
 
 const clientValid = (btoa(clientId + ':' + clientSecret))
 let tokenURL = 'https://accounts.spotify.com/api/token';
-let artistURL = 'https://api.spotify.com/v1/search?q=Daft+Punk&type=artist&limit=1';
+let artistURL = 'https://api.spotify.com/v1/search?q=' + 'Daft+Punk' + '&type=artist&limit=1';
 
 
 
@@ -55,27 +55,7 @@ const getTopTracks = async () => {
     })
     const data = await response.json();
     console.log(data);
-    //sessionStorage.setItem("", )
+    //set top songs to element with url link to another api call for lyrics/audio demo
 };
 
 getTopTracks ();
-
-//let topTracks = sessionStorage.getItem("");
-
-    /*.then(res => {
-    return res.json()
-})
-.then(data => console.log(data))
-.catch(error => console.log('ERROR'))
-let result = JSON.parse(data);
-
-console.log(data.access_token);
-
-
-console.log(data)
-
- const data = result.json();
-return data.acces_token;  
-
-    
-console.log(result)*/
