7d63e78098ad26966a17e84e5ce12780c2be9ca5
beca_d <67708213+Beca-d@users.noreply.github.com>
2022-06-20 18:00:22 -0400

    organized functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,31 @@
+/* 
+1. Set Variables
+2. Generate Token
+3. Use search input to find artist's ID
+4. If no artist found open modal stating "try again" add close button to modal 
+5. Save search to local Storage
+6. Get Search history from local storage 
+7. Add search history to list element
+8. Use artist's ID to return Top Tracks
+9. Add top tracks to list element
+10. Generate Lyrics when top track selected
+11. Start Audio player when top track selected
+12. Open Modal with Lyrics when Copy Lyrics Selected
+13. Copy Lyrics to clipboard when copy button cicked in open Modal
+14. Close modal when "X" or close button clicked
+15. Clear Search History 
+*/
+
+// 1. *************************************************
 const clientId = '0a772d470d6b4bc295787c115dc84e0e';
 const clientSecret = '51705cf2746340e3b66493d943e6eb05';
-
 const clientValid = (btoa(clientId + ':' + clientSecret))
 let tokenURL = 'https://accounts.spotify.com/api/token';
-let artistURL = 'https://api.spotify.com/v1/search?q=' + 'Daft+Punk' + '&type=artist&limit=1';
-//need to add variable to artistURL to bring in search function 
+// Selector for search button
+const searchButtonEl = $(".search-button"); 
+let searchHistory = [];
 
+// 2. ****************************************************************
 //Must get token api call to run first - otherwise errors occur!!!!
 const getToken = async () => {
     const response = await fetch(tokenURL, {
@@ -24,8 +44,21 @@ const getToken = async () => {
 getToken();
 
 let token = sessionStorage.getItem("token");
+console.log(token);
 
+setInterval(getToken, 3600000);
+
+searchButtonEl.on("click", function() {
+    const searchText = $(".search-bar").val();
+
+    console.log(searchText);
+    getArtistID(searchText);
+    // renderTopSongs(trackNames);
+});
+
+// 3.a) ****************************************************
 const getArtistID = async () => {
+    const artistURL = 'https://api.spotify.com/v1/search?q=' + 'queen' + '&type=artist&limit=1';
     const response = await fetch(artistURL, {
         method: 'GET',
         headers: {
@@ -36,15 +69,19 @@ const getArtistID = async () => {
     })
     const data = await response.json();
     //console.log(data.artists.items[0].id);
-    sessionStorage.setItem("artistId", data.artists.items[0].id)
+    let artistId = data.artists.items[0].id
+    console.log(artistId);
+    sessionStorage.setItem("artistId", artistId);
+
+    // 8.b) Call getTopTracks within Atist ID func to have access to artistId ****
+    getTopTracks ();
 };
 
-getArtistID ();
 
-let artistId = sessionStorage.getItem("artistId");
-let songsURL = 'https://api.spotify.com/v1/artists/' + artistId + '/top-tracks?market=CA';
-
-const getTopTracks = async () => {
+ // 8.a) function to get Top Tracks *********************************
+ const getTopTracks = async () => {
+    let artistId = sessionStorage.getItem("artistId");
+    let songsURL = 'https://api.spotify.com/v1/artists/' + artistId + '/top-tracks?market=CA';
     const response = await fetch(songsURL, {
         method: 'GET',
         headers: {
@@ -60,94 +97,20 @@ const getTopTracks = async () => {
     sessionStorage.setItem("trackNames", JSON.stringify(trackNames));
     sessionStorage.setItem("trackIds", JSON.stringify(trackIds));
     //set top songs to element with url link to another api call for lyrics/audio demo
+
+    renderTopSongs(trackNames, trackIds);
+    console.log(trackNames)
 };
 
-getTopTracks ();
-let trackNames = JSON.parse(sessionStorage.getItem("trackNames"));
-let trackIds = JSON.parse(sessionStorage.getItem("trackIds"));
-//console.log(trackIds);
+// 3.b) Call Artist Id func ************************************
+getArtistID ();
 
-document.getElementById('song0').innerHTML = trackNames[0];
-document.getElementById('song1').innerHTML = trackNames[1];
-document.getElementById('song2').innerHTML = trackNames[2];
-document.getElementById('song3').innerHTML = trackNames[3];
-document.getElementById('song4').innerHTML = trackNames[4];
-document.getElementById('song5').innerHTML = trackNames[5];
-document.getElementById('song6').innerHTML = trackNames[6];
-document.getElementById('song7').innerHTML = trackNames[7];
-document.getElementById('song8').innerHTML = trackNames[8];
-document.getElementById('song9').innerHTML = trackNames[9];
+// 4. ********************************************************
+// No Artist found open Modal!!
 
-//document.getElementById('song0').href = "https://open.spotify.com/embed/track/" + trackIds[0];
-//document.getElementById('song1').href = "https://open.spotify.com/embed/track/" + trackIds[1];
-//document.getElementById('song2').href = "https://open.spotify.com/embed/track/" + trackIds[2];
-//document.getElementById('song3').href = "https://open.spotify.com/embed/track/" + trackIds[3];
-///document.getElementById('song4').href = "https://open.spotify.com/embed/track/" + trackIds[4];
-//document.getElementById('song5').href = "https://open.spotify.com/embed/track/" + trackIds[5];
-//document.getElementById('song6').href = "https://open.spotify.com/embed/track/" + trackIds[6];
-//document.getElementById('song7').href = "https://open.spotify.com/embed/track/" + trackIds[7];
-//document.getElementById('song8').href = "https://open.spotify.com/embed/track/" + trackIds[8];
-//document.getElementById('song9').href = "https://open.spotify.com/embed/track/" + trackIds[9];
 
-const playSong0 = document.querySelector("#song0");
-    playSong0.addEventListener('click', (e) => {
-        document.getElementById('music-player').src = "https://open.spotify.com/embed/track/" + trackIds[0] + "?utm_source=generator"
-});
+// 5. *********************************************************
 
-const playSong1 = document.querySelector("#song1");
-    playSong0.addEventListener('click', (e) => {
-        document.getElementById('music-player').src = "https://open.spotify.com/embed/track/" + trackIds[1] + "?utm_source=generator"
-});
-
-const playSong2 = document.querySelector("#song2");
-    playSong0.addEventListener('click', (e) => {
-        document.getElementById('music-player').src = "https://open.spotify.com/embed/track/" + trackIds[2] + "?utm_source=generator"
-});
-
-const playSong3 = document.querySelector("#song3");
-    playSong0.addEventListener('click', (e) => {
-        document.getElementById('music-player').src = "https://open.spotify.com/embed/track/" + trackIds[3] + "?utm_source=generator"
-});
-
-const playSong4 = document.querySelector("#song4");
-    playSong0.addEventListener('click', (e) => {
-        document.getElementById('music-player').src = "https://open.spotify.com/embed/track/" + trackIds[4] + "?utm_source=generator"
-});
-
-const playSong5 = document.querySelector("#song5");
-    playSong0.addEventListener('click', (e) => {
-        document.getElementById('music-player').src = "https://open.spotify.com/embed/track/" + trackIds[5] + "?utm_source=generator"
-});
-
-const playSong6 = document.querySelector("#song6");
-    playSong0.addEventListener('click', (e) => {
-        document.getElementById('music-player').src = "https://open.spotify.com/embed/track/" + trackIds[6] + "?utm_source=generator"
-});
-
-const playSong7 = document.querySelector("#song7");
-    playSong0.addEventListener('click', (e) => {
-        document.getElementById('music-player').src = "https://open.spotify.com/embed/track/" + trackIds[7] + "?utm_source=generator"
-});
-
-const playSong8 = document.querySelector("#song8");
-    playSong0.addEventListener('click', (e) => {
-        document.getElementById('music-player').src = "https://open.spotify.com/embed/track/" + trackIds[8] + "?utm_source=generator"
-});
-
-const playSong9 = document.querySelector("#song9");
-    playSong0.addEventListener('click', (e) => {
-        document.getElementById('music-player').src = "https://open.spotify.com/embed/track/" + trackIds[9] + "?utm_source=generator"
-});
-
-// Jaryd's work **
-let searchHistory = [];
-
-// Function to get search history from local storage.
-const getSearchHistory = () => {
-    if (localStorage.getItem("search-history")) {
-        searchHistory = JSON.parse(localStorage.getItem("search-history"));
-    }
-};
 
 // Function to save search history
 const saveSearchHistory = () => {
@@ -158,12 +121,14 @@ const saveSearchHistory = () => {
     }
 };
 
-// Function to clear search history
-const clearSearchHistory = () => {
-    searchHistory = [];
-    localStorage.removeItem("search-history");
+// 6. ************************************************************
+// Function to get search history from local storage.
+const getSearchHistory = () => {
+    if (localStorage.getItem("search-history")) {
+        searchHistory = JSON.parse(localStorage.getItem("search-history"));
+    }
 };
-
+ // 7. ******************************************************************
 // Function to render search history
 const renderSearchHistory = () => {
     const searchHistoryEl = $("#search-history").children(".menu-list");
@@ -173,3 +138,40 @@ const renderSearchHistory = () => {
         searchHistoryEl.append(`<li><a>${item}</a></li>`)
     })
 };
+
+// Replaced Code with more effecient Arrays******************************************
+//let trackNames = JSON.parse(sessionStorage.getItem("trackNames"));
+//let trackIds = JSON.parse(sessionStorage.getItem("trackIds"));
+//console.log(trackIds);
+//document.getElementById('song0').innerHTML = trackNames[0];
+//document.getElementById('song0').href = "https://open.spotify.com/embed/track/" + trackIds[0];
+/*const playSong0 = document.querySelector("#song0");
+    playSong0.addEventListener('click', (e) => {
+        document.getElementById('music-player').src = "https://open.spotify.com/embed/track/" + trackIds[0] + "?utm_source=generator"
+}); */
+
+// 9. Add top tracks to list *****************************************************
+// Selector for top songs list
+const topSongListEl = $(".top-song-list");
+
+const renderTopSongs = (arr, trackNames) => {
+    let counter = 0;
+    topSongListEl.text("");
+    arr.forEach(song => {
+        topSongListEl.append(`<li class='song${counter}' data-track='${trackNames[counter]}'>${song}</li>`);
+        counter ++;
+    });
+};
+
+// 11. Top Song Player ********************************************************
+topSongListEl.on("click", "li", function (event) {
+    $("#music-player").attr("src", `https://open.spotify.com/embed/track/${event.target.getAttribute("data-track")}`);
+})
+
+// 15. ************************************************************************
+// Function to clear search history
+const clearSearchHistory = () => {
+    searchHistory = [];
+    localStorage.removeItem("search-history");
+};
+
