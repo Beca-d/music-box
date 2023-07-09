93edd0ec5f5d9e9ea3a84703bd8745e5e4d340a4
Michelle <michelle.douma@gmail.com>
2023-07-09 19:05:49 -0400

    update keys

--- assets/js/lyrics.js
+++ assets/js/lyrics.js
@@ -1,115 +1,116 @@
 var selectedTrack = document.getElementById("track-list");
 var lyricsContainer = document.getElementById("lyrics");
 var artistEl = document.getElementById("artist-list");
-var lyricsModal = document.getElementById('song-lyrics-modal');
+var lyricsModal = document.getElementById("song-lyrics-modal");
 
 const lyricsNotFoundModalEl = $("#modal-lyrics-not-found");
 
-// Get Lyrics Lyrics.ovh API 
+// Get Lyrics Lyrics.ovh API
 const songID = async (event) => {
-  
-    let track = event.target.textContent.trim();
-    let isFeatTrack = track.indexOf('(') !== -1 ? track.split('(')[0].trim() : track;
-    let artist = artistEl.firstElementChild.textContent;
-    // calling API
-    try {
-        await fetch(`https://api.lyrics.ovh/v1/${artist}/${isFeatTrack}`, {
-            method: "GET",
-            mode: 'cors',
-            headers: {}
-        })
-            .then(response => {
-                return isResponseOK(response)
-            })
-            .then(data => {
-                let result = isEmptyOrNull(data);
+	let track = event.target.textContent.trim();
+	let isFeatTrack =
+		track.indexOf("(") !== -1 ? track.split("(")[0].trim() : track;
+	let artist = artistEl.firstElementChild.textContent;
+	// calling API
+	try {
+		await fetch(`https://api.lyrics.ovh/v1/${artist}/${isFeatTrack}`, {
+			method: "GET",
+			mode: "cors",
+			headers: {},
+		})
+			.then((response) => {
+				return isResponseOK(response);
+			})
+			.then((data) => {
+				let result = isEmptyOrNull(data);
 
-                // Setting the value to Lyrics Container
-                lyricsContainer.querySelector("div").innerText = "";
-                lyricsContainer.querySelector("div").innerText = result.lyrics;
-                lyricsModal.innerText = result.lyrics;
-            })
-
-    } catch (error) {
-        lyricsContainer.querySelector("div").innerText = "";
-        lyricsNotFoundModalEl.addClass("is-active")
-    }
+				// Setting the value to Lyrics Container
+				lyricsContainer.querySelector("div").innerText = "";
+				lyricsContainer.querySelector("div").innerText = result.lyrics;
+				lyricsModal.innerText = result.lyrics;
+			});
+	} catch (error) {
+		lyricsContainer.querySelector("div").innerText = "";
+		lyricsNotFoundModalEl.addClass("is-active");
+	}
 };
 
 // Response Validation
 var isResponseOK = (response) => {
-    if (!response.ok)
-        throw Error('No Lyrics Found !');
-    else
-        return response.json();
-}
+	if (!response.ok) throw Error("No Lyrics Found !");
+	else return response.json();
+};
 
 // Data Validation
 var isEmptyOrNull = (data) => {
-    if (data === null || data === '')
-        throw Error("No Records Found !!");
-    else
-        return data;
-}
+	if (data === null || data === "") throw Error("No Records Found !!");
+	else return data;
+};
 
 // Event will trigger on selecting the track
 selectedTrack.addEventListener("click", songID);
 
 // Functions to control Lyrics Modal!
-document.addEventListener('DOMContentLoaded', () => {
-    // Functions to open and close a modal
-    function openModal($el) {
-      $el.classList.add('is-active');
-    }
-  
-    function closeModal($el) {
-      $el.classList.remove('is-active');
-    }
-  
-    function closeAllModals() {
-      (document.querySelectorAll('.modal') || []).forEach(($modal) => {
-        closeModal($modal);
-      });
-    }
-  
-    // Add a click event on buttons to open a specific modal
-    (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
-      const modal = $trigger.dataset.target;
-      const $target = document.getElementById(modal);
-  
-      $trigger.addEventListener('click', () => {
-        openModal($target);
-      });
-    });
-  
-    // Add a click event on various child elements to close the parent modal
-    (document.querySelectorAll('.modal-background, .close-lyrics, .close-lyrics-not-found') || []).forEach(($close) => {
-      const $target = $close.closest('.modal');
-  
-      $close.addEventListener('click', () => {
-        closeModal($target);
-      });
-    });
-  
-    // Add a keyboard event to close all modals
-    document.addEventListener('keydown', (event) => {
-      const e = event || window.event;
-  
-      if (e.keyCode === 27) { // Escape key
-        closeAllModals();
-      }
-    });
-  });
+document.addEventListener("DOMContentLoaded", () => {
+	// Functions to open and close a modal
+	function openModal($el) {
+		$el.classList.add("is-active");
+	}
+
+	function closeModal($el) {
+		$el.classList.remove("is-active");
+	}
+
+	function closeAllModals() {
+		(document.querySelectorAll(".modal") || []).forEach(($modal) => {
+			closeModal($modal);
+		});
+	}
+
+	// Add a click event on buttons to open a specific modal
+	(document.querySelectorAll(".js-modal-trigger") || []).forEach(($trigger) => {
+		const modal = $trigger.dataset.target;
+		const $target = document.getElementById(modal);
+
+		$trigger.addEventListener("click", () => {
+			openModal($target);
+		});
+	});
+
+	// Add a click event on various child elements to close the parent modal
+	(
+		document.querySelectorAll(
+			".modal-background, .close-lyrics, .close-lyrics-not-found"
+		) || []
+	).forEach(($close) => {
+		const $target = $close.closest(".modal");
+
+		$close.addEventListener("click", () => {
+			closeModal($target);
+		});
+	});
+
+	// Add a keyboard event to close all modals
+	document.addEventListener("keydown", (event) => {
+		const e = event || window.event;
+
+		if (e.keyCode === 27) {
+			// Escape key
+			closeAllModals();
+		}
+	});
+});
 
 // Add a click event to copy modal content to clipboard
 document.querySelector(".copier").onclick = function () {
-    const _this = this
-    navigator.clipboard.writeText(document.getElementById('song-lyrics-modal').innerText)
-    .then(function() {
-        _this.innerText = 'Copied'
+	const _this = this;
+	navigator.clipboard
+		.writeText(document.getElementById("song-lyrics-modal").innerText)
+		.then(function () {
+			_this.innerText = "Copied";
 
-        setTimeout(function() {
-            _this.innerHTML = '<i class="fas fa-copy"></i>' 
-        }, 1000)
-        })
-}
+			setTimeout(function () {
+				_this.innerHTML = '<i class="fas fa-copy"></i>';
+			}, 1000);
+		});
+};
--- assets/js/script.js
+++ assets/js/script.js
@@ -17,10 +17,10 @@
 */
 
 // 1. *************************************************
-const clientId = '0a772d470d6b4bc295787c115dc84e0e';
-const clientSecret = '51705cf2746340e3b66493d943e6eb05';
-const clientValid = (btoa(clientId + ':' + clientSecret))
-let tokenURL = 'https://accounts.spotify.com/api/token';
+const clientId = "e2b5358081e4411ba962baa3360d6d2b";
+const clientSecret = "0b90308f80b44e5da3910d3b33279b8e";
+const clientValid = btoa(clientId + ":" + clientSecret);
+let tokenURL = "https://accounts.spotify.com/api/token";
 // Selector for search button
 const searchButtonEl = $(".search-button");
 const clearHistoryButtonEl = $(".clear-history");
@@ -33,114 +33,135 @@ let searchText = "";
 // 2. ****************************************************************
 //Must get token api call to run first - otherwise errors occur!!!!
 const getToken = async (searchValueFromHistory) => {
-    const response = await fetch(tokenURL, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Authorization': 'Basic ' + clientValid
-        },
-        body: 'grant_type=client_credentials'
-    })
-    const data = await response.json();
-    const { access_token } = data;
-    // sessionStorage.setItem("token", access_token);
-    let input = searchValueFromHistory ? searchValueFromHistory : searchText;
-    getArtistID(input, access_token);
+	const response = await fetch(tokenURL, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded",
+			Authorization: "Basic " + clientValid,
+		},
+		body: "grant_type=client_credentials",
+	});
+	const data = await response.json();
+	const { access_token } = data;
+	// sessionStorage.setItem("token", access_token);
+	let input = searchValueFromHistory ? searchValueFromHistory : searchText;
+	getArtistID(input, access_token);
 };
 
 searchButtonEl.on("click", function () {
+	// Re-setting the value of the search bar
+	searchText = $(".search-bar").val().trim();
+	$(".search-bar").val("");
+	$(".search-bar").attr("placeholder", "Artist name");
 
-    // Re-setting the value of the search bar
-    searchText = $(".search-bar").val().trim();
-    $(".search-bar").val('');
-    $(".search-bar").attr("placeholder", "Artist name");
-
-    // Only trigger if the value is not blank
-    if (searchText != "") {
-        getToken();
-    }
+	// Only trigger if the value is not blank
+	if (searchText != "") {
+		getToken();
+	}
 });
-
 
 // 3.a) ****************************************************
 const getArtistID = async (artist, token) => {
-    const artistURL = `https://api.spotify.com/v1/search?q=${artist}&type=artist&limit=1`;
-    const response = await fetch(artistURL, {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'aplication/json',
-            'Authorization': 'Bearer ' + token
-        }
-    })
-    const data = await response.json();
+	const artistURL = `https://api.spotify.com/v1/search?q=${artist}&type=artist&limit=1`;
+	const response = await fetch(artistURL, {
+		method: "GET",
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "aplication/json",
+			Authorization: "Bearer " + token,
+		},
+	});
+	const data = await response.json();
 
-    const artistId = data.artists.items[0].id;
+	const artistId = data.artists.items[0].id;
 
-    // 8.b) Call getTopTracks within Atist ID func to have access to artistId ****
-    getTopTracks(artistId, token);
+	// 8.b) Call getTopTracks within Atist ID func to have access to artistId ****
+	getTopTracks(artistId, token);
 
-    const artistName = document.getElementById("artist-name")
-    artistName.innerText = artist.toUpperCase();
+	const artistName = document.getElementById("artist-name");
+	artistName.innerText = artist.toUpperCase();
 };
 
 // Function to get top tracks
 const getTopTracks = async (artistId, token) => {
-    let songsURL = 'https://api.spotify.com/v1/artists/' + artistId + '/top-tracks?market=CA';
-    const response = await fetch(songsURL, {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'aplication/json',
-            'Authorization': 'Bearer ' + token
-        }
-    })
-    const data = await response.json();
-    const { tracks } = data;
-    var trackNames = [tracks[0].name, tracks[1].name, tracks[2].name, tracks[3].name, tracks[4].name, tracks[5].name, tracks[6].name, tracks[7].name, tracks[8].name, tracks[9].name];
-    var trackIds = [tracks[0].id, tracks[1].id, tracks[2].id, tracks[3].id, tracks[4].id, tracks[5].id, tracks[6].id, tracks[7].id, tracks[8].id, tracks[9].id];
+	let songsURL =
+		"https://api.spotify.com/v1/artists/" + artistId + "/top-tracks?market=CA";
+	const response = await fetch(songsURL, {
+		method: "GET",
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "aplication/json",
+			Authorization: "Bearer " + token,
+		},
+	});
+	const data = await response.json();
+	const { tracks } = data;
+	var trackNames = [
+		tracks[0].name,
+		tracks[1].name,
+		tracks[2].name,
+		tracks[3].name,
+		tracks[4].name,
+		tracks[5].name,
+		tracks[6].name,
+		tracks[7].name,
+		tracks[8].name,
+		tracks[9].name,
+	];
+	var trackIds = [
+		tracks[0].id,
+		tracks[1].id,
+		tracks[2].id,
+		tracks[3].id,
+		tracks[4].id,
+		tracks[5].id,
+		tracks[6].id,
+		tracks[7].id,
+		tracks[8].id,
+		tracks[9].id,
+	];
 
-    //set top songs to element with url link to another api call for lyrics/audio demo
+	//set top songs to element with url link to another api call for lyrics/audio demo
 
-    renderTopSongs(trackNames, trackIds);
-    saveSearchHistory();
-    renderSearchHistory();
+	renderTopSongs(trackNames, trackIds);
+	saveSearchHistory();
+	renderSearchHistory();
 };
 
 // 4. ********************************************************
 // No Artist found open Modal!!
 
-
 // 5. *********************************************************
-
 
 // Function to save search history
 const saveSearchHistory = () => {
-    // const searchValue = $(".search-bar").val().trim().toUpperCase()
-    if (searchText !="" && !searchHistory.includes(searchText.toUpperCase())) {
-        searchHistory.push(searchText.toUpperCase());
-        localStorage.setItem("search-history", JSON.stringify(searchHistory));
-    }
+	// const searchValue = $(".search-bar").val().trim().toUpperCase()
+	if (searchText != "" && !searchHistory.includes(searchText.toUpperCase())) {
+		searchHistory.push(searchText.toUpperCase());
+		localStorage.setItem("search-history", JSON.stringify(searchHistory));
+	}
 };
 
 // 6. ************************************************************
 // Function to get search history from local storage.
 const getSearchHistory = () => {
-    if (localStorage.getItem("search-history")) {
-        searchHistory = JSON.parse(localStorage.getItem("search-history"));
-    }
+	if (localStorage.getItem("search-history")) {
+		searchHistory = JSON.parse(localStorage.getItem("search-history"));
+	}
 };
 // 7. ******************************************************************
 // Function to render search history
 const renderSearchHistory = () => {
-    const searchHistoryEl = $("#search-history").children(".menu-list");
-    searchHistoryEl.empty();
+	const searchHistoryEl = $("#search-history").children(".menu-list");
+	searchHistoryEl.empty();
 
-    searchHistory.forEach(item => {
-        if (item != "") {
-            searchHistoryEl.prepend(`<li><a class='search-history-item'>${item}</a></li>`)
-        }
-    })
+	searchHistory.forEach((item) => {
+		if (item != "") {
+			searchHistoryEl.prepend(
+				`<li><a class='search-history-item'>${item}</a></li>`
+			);
+		}
+	});
 };
 
 // 9. Add top tracks to list *****************************************************
@@ -148,70 +169,83 @@ const renderSearchHistory = () => {
 const topSongListEl = $(".top-song-list");
 
 const renderTopSongs = (arr, trackIds) => {
-    let counter = 0;
-    topSongListEl.text("");
-    arr.forEach(song => {
-        topSongListEl.append(`<li class='song${counter}' data-track='${trackIds[counter]}'>${song}</li>`);
-        counter++;
-    });
+	let counter = 0;
+	topSongListEl.text("");
+	arr.forEach((song) => {
+		topSongListEl.append(
+			`<li class='song${counter}' data-track='${trackIds[counter]}'>${song}</li>`
+		);
+		counter++;
+	});
 };
 
 // 11. Top Song Player ********************************************************
-// Event Listener for click on top song. So far it opens the iFrame for the song sample. 
+// Event Listener for click on top song. So far it opens the iFrame for the song sample.
 topSongListEl.on("click", "li", function (event) {
-    //const spanSong = document.getElementById("span-title");
-    //spanSong.innerText = "Song";
-    
-    $("#placeholder").remove();
+	//const spanSong = document.getElementById("span-title");
+	//spanSong.innerText = "Song";
 
-    $("#music-player").attr("src", `https://open.spotify.com/embed/track/${event.target.getAttribute("data-track")}`);
-    $("#full-song-url").attr("href", `https://open.spotify.com/embed/track/${event.target.getAttribute("data-track")}`);
+	$("#placeholder").remove();
+
+	$("#music-player").attr(
+		"src",
+		`https://open.spotify.com/embed/track/${event.target.getAttribute(
+			"data-track"
+		)}`
+	);
+	$("#full-song-url").attr(
+		"href",
+		`https://open.spotify.com/embed/track/${event.target.getAttribute(
+			"data-track"
+		)}`
+	);
 });
 
 // Event listener for clicks on the clear history button
 clearHistoryButtonEl.on("click", function (event) {
-    clearSearchHistory();
-    renderSearchHistory();
-})
+	clearSearchHistory();
+	renderSearchHistory();
+});
 
 // Event listener for clicks on search history item to bring up results of search again
 searchHistoryListEl.on("click", "li", function (event) {
-    const searchText = $(event.target).text();
-    let index = searchHistory.indexOf(searchText);
+	const searchText = $(event.target).text();
+	let index = searchHistory.indexOf(searchText);
 
-    // Switching Tabs in Search History
-    if (searchHistory.length > 0 && index > -1) {
-        searchHistory.splice(index, 1);
-        searchHistory.push(searchText);
-        localStorage.setItem("search-history", JSON.stringify(searchHistory));
-    }
-    getToken(searchText);
-})
+	// Switching Tabs in Search History
+	if (searchHistory.length > 0 && index > -1) {
+		searchHistory.splice(index, 1);
+		searchHistory.push(searchText);
+		localStorage.setItem("search-history", JSON.stringify(searchHistory));
+	}
+	getToken(searchText);
+});
 
 // 15. ************************************************************************
 // Function to clear search history
 const clearSearchHistory = () => {
-    searchHistory = [];
-    $(topSongListEl).empty();
-    localStorage.removeItem("search-history");
+	searchHistory = [];
+	$(topSongListEl).empty();
+	localStorage.removeItem("search-history");
 
-    // Reset all the container
-    lyricsContainer.querySelector("div").innerText = "Click on a song to display Lyrics!";
-    $("#music-player").attr("src", "");
-    $("#music-player").empty();
-    $(".music-player-container").find("img").remove()
-    $('#music-player').before(`<img src="./assets/images/musicplayer.jpg" id="placeholder" alt="Music Player Placeholder" />`)
+	// Reset all the container
+	lyricsContainer.querySelector("div").innerText =
+		"Click on a song to display Lyrics!";
+	$("#music-player").attr("src", "");
+	$("#music-player").empty();
+	$(".music-player-container").find("img").remove();
+	$("#music-player").before(
+		`<img src="./assets/images/musicplayer.jpg" id="placeholder" alt="Music Player Placeholder" />`
+	);
 };
 
 // On Load Function
 const init = (() => {
+	getSearchHistory();
+	renderSearchHistory();
 
-    getSearchHistory();
-    renderSearchHistory();
-
-    // Loading the Top Songs on page re-load
-    if (searchHistory.length > 0) {
-       searchHistoryListEl.find("li").eq(0).trigger("click")
-    }
+	// Loading the Top Songs on page re-load
+	if (searchHistory.length > 0) {
+		searchHistoryListEl.find("li").eq(0).trigger("click");
+	}
 })();
-
